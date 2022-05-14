02030c581fa558b1b460686921217a5652dfe999
ncn000 <ncn949494@gmail.com>
2022-05-14 13:18:24 -0700

    useKeyboard custom hook tests

--- hooks/useKeyboard/useKeyboard.test.js
+++ hooks/useKeyboard/useKeyboard.test.js
@@ -1,28 +1,65 @@
-import {renderHook} from "@testing-library/react-hooks";
+import {act, renderHook} from "@testing-library/react-hooks";
 import {useKeyboard} from "./useKeyboard";
 
+const dispatchKeyboardEvent = (eventDict) => {
+    document.dispatchEvent(new KeyboardEvent('keydown', eventDict));
+}
+
 describe("useKeyboard custom hook", () => {
-    const initialProps = [{
-        key: "f", mods: ["ctrl"], action: (e) => {
-            e.preventDefault();
-            document.querySelector("#filter-searchbar").focus();
+    const initialPhrase = "mod g";
+    const secondPhrase = "no mod g";
+    let props = [
+        {
+            key: "g", mods: ["ctrl"], action: (e) => {
+                e.preventDefault();
+                console.log(initialPhrase);
+            }
+        },
+        {
+            key: "g", action: (e) => {
+                e.preventDefault();
+                console.log(secondPhrase);
+            }
         }
-    }]
+    ]
 
+    const logSpy = jest.spyOn(console, 'log');
 
-    test('should be consumable', () => {
-        const {result} = renderHook(() => useKeyboard(initialProps));
-
-        expect(typeof result.current).toBe('function');
+    beforeEach(() => {
+        renderHook(() => useKeyboard(props));
+        logSpy.mockClear();
     })
-    //
-    // test("should increment", () => {
-    //     const {result} = renderHook(() => useCounter());
-    //
-    //     act(() => {
-    //         result.current.increment();
-    //     })
-    //
-    //     expect(result.current.count).toBe(1);
-    // })
+
+    test('should not be called', () => {
+        act(() => {
+            dispatchKeyboardEvent({
+                key: "f",
+                ctrlKey: false,
+            })
+        })
+
+        expect(logSpy).not.toHaveBeenCalled();
+    })
+
+    test('"mod g" should be called in the console', () => {
+        act(() => {
+            dispatchKeyboardEvent({
+                key: "g",
+                ctrlKey: true,
+            })
+        })
+
+        expect(logSpy).toHaveBeenCalledWith(initialPhrase);
+    })
+
+    test('"no mod g" should be called in the console', () => {
+        act(() => {
+            dispatchKeyboardEvent({
+                key: "g",
+                ctrlKey: false,
+            })
+        })
+
+        expect(logSpy).toHaveBeenCalledWith(secondPhrase);
+    })
 })
